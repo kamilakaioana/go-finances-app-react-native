5e16567adaea7bbe4bb69fde32fe955f3420333e
Kamila Kaioana Aleixo Costa <kamilaaleixocosta@hotmail.com>
2021-08-07 18:10:04 -0300

    app projeto go finances

---
+++ App.tsx
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import {
+useFonts,
+Poppins_400Regular, 
+Poppins_500Medium,
+Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+// import DashBoard from './src/screens/DashBoard';
+import AppLoading from 'expo-app-loading';
+
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
+import Register from './src/screens/Register';
+
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts ({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+  return (
+  <ThemeProvider theme={theme}>
+    {/* <DashBoard /> */}
+    <Register />
+
+  </ThemeProvider>
+  )
+  
+  
+  
+ 
+
+}
+
